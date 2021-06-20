ff049523dfb27565294b4b40271f89d4489dc702
Rudi <rudivisagie@icloud.com>
2021-06-20 15:29:03 +0000

    added certificate creation command step 1.4

--- roles/ca/files/makedb.sh
+++ roles/ca/files/makedb.sh
@@ -5,3 +5,4 @@ echo 01 > ca/root-ca/db/root-ca.crt.srl
 echo 01 > ca/root-ca/db/root-ca.crl.srl
 
 openssl req -new -config ./root-ca.cnf -out ca/root-ca.csr -keyout ca/root-ca/private/root-ca.key
+openssl ca -batch -selfsign -config ./root-ca.cnf -in ca/root-ca.csr -out ca/root-ca.crt -extensions root_ca_ext
